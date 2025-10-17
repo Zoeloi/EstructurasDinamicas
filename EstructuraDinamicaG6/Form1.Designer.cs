c4822e94dc075357b73b2b1d9f00c0e2c717e560
Zoeloi <eaalmendarez@uamv.edu.ni>
2025-10-17 10:39:27 -0600

    Ejercicio Pilas Resuelto

--- EstructuraDinamicaG6/Form1.Designer.cs
+++ EstructuraDinamicaG6/Form1.Designer.cs
@@ -28,12 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            tbEdad = new TextBox();
+            btnAgregar = new Button();
+            lblEdades = new Label();
+            btnSacar = new Button();
+            btnMostrar = new Button();
+            btnOrdenar = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 26);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Edad:";
+            // 
+            // tbEdad
+            // 
+            tbEdad.Location = new Point(91, 23);
+            tbEdad.Name = "tbEdad";
+            tbEdad.Size = new Size(186, 27);
+            tbEdad.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(311, 12);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(160, 75);
+            btnAgregar.TabIndex = 2;
+            btnAgregar.Text = "Agregar";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += button1_Click;
+            // 
+            // lblEdades
+            // 
+            lblEdades.AutoSize = true;
+            lblEdades.Location = new Point(28, 166);
+            lblEdades.Name = "lblEdades";
+            lblEdades.Size = new Size(57, 20);
+            lblEdades.TabIndex = 3;
+            lblEdades.Text = "edades";
+            // 
+            // btnSacar
+            // 
+            btnSacar.Location = new Point(505, 12);
+            btnSacar.Name = "btnSacar";
+            btnSacar.Size = new Size(139, 75);
+            btnSacar.TabIndex = 4;
+            btnSacar.Text = "Sacar";
+            btnSacar.UseVisualStyleBackColor = true;
+            btnSacar.Click += btnSacar_Click;
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.Location = new Point(675, 12);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(132, 75);
+            btnMostrar.TabIndex = 5;
+            btnMostrar.Text = "Mostrar Ultimo";
+            btnMostrar.UseVisualStyleBackColor = true;
+            btnMostrar.Click += btnMostrar_Click;
+            // 
+            // btnOrdenar
+            // 
+            btnOrdenar.Location = new Point(832, 12);
+            btnOrdenar.Name = "btnOrdenar";
+            btnOrdenar.Size = new Size(110, 75);
+            btnOrdenar.TabIndex = 6;
+            btnOrdenar.Text = "Ordenar";
+            btnOrdenar.UseVisualStyleBackColor = true;
+            btnOrdenar.Click += btnOrdenar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1086, 450);
+            Controls.Add(btnOrdenar);
+            Controls.Add(btnMostrar);
+            Controls.Add(btnSacar);
+            Controls.Add(lblEdades);
+            Controls.Add(btnAgregar);
+            Controls.Add(tbEdad);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox tbEdad;
+        private Button btnAgregar;
+        private Label lblEdades;
+        private Button btnSacar;
+        private Button btnMostrar;
+        private Button btnOrdenar;
     }
 }
--- EstructuraDinamicaG6/Form1.cs
+++ EstructuraDinamicaG6/Form1.cs
@@ -1,10 +1,91 @@
+using System.Windows.Forms;
+
 namespace EstructuraDinamicaG6
 {
     public partial class Form1 : Form
     {
+
+        Stack<int> pila = new Stack<int>();
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int edad = int.Parse(tbEdad.Text);
+            pila.Push(edad);
+            Mostrar();
+        }
+        private void Mostrar()
+        {
+            string mensaje = "";
+            foreach (int edad in pila)
+            {
+                mensaje += "" + edad + "/";
+            }
+            lblEdades.Text = mensaje;
+        }
+
+        private void btnSacar_Click(object sender, EventArgs e)
+        {
+            if (pila.Count > 0)
+            {
+                pila.Pop();
+                Mostrar();
+
+            }
+            else
+            {
+                MessageBox.Show("La pila está vacía");
+            }
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            if (pila.Count > 0)
+            {
+                MessageBox.Show("El elemento en la cima es: " + pila.Peek());
+            }
+            else
+            {
+                MessageBox.Show("La pila está vacía");
+            }
+        }
+
+
+        //Ordenar de mayor a menor en el label lblEdades
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            OrdenarPila();
+        }
+
+
+        private void OrdenarPila()
+        {
+
+            
+            Stack <int> pila2 = new Stack<int>();
+
+
+            while(pila.Count > 0)
+            {
+                int temp = pila.Pop();
+                
+                while (pila.Count > 0 && pila2.Peek() > temp)
+                {
+                    pila.Push(pila2.Pop() );
+                }
+                pila2.Push(temp);
+            }
+            pila = pila2;
+           
+         }
+
+        
+      
+
     }
 }
